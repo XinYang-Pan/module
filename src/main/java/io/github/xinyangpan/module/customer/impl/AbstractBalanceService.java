12f3967ac8dd7dfc993266f4f9232f16ab9886a7
Pan, Xinyang <seanpan2008@gmail.com>
2017-07-18 17:27:41 +0800

    polishing

--- src/main/java/io/github/xinyangpan/module/customer/impl/AbstractBalanceService.java
+++
@@ -1,9 +0,0 @@
-package io.github.xinyangpan.module.customer.impl;
-
-import io.github.xinyangpan.module.customer.CustomerBalanceService;
-
-public abstract class AbstractBalanceService implements CustomerBalanceService {
-	
-	
-	
-}
